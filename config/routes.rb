13c5c05b79d1cf7bec78e4d6ccb39ce13762ef40
Roman Korobko <romakorobko@gmail.com>
2018-11-13 09:46:21 +0200

    add user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   resources :projects do
     resources :tasks
   end
+
+  post 'auth/sign_in', to: 'authentication#authenticate'
+
+  post 'auth', to: 'users#create'
 end
